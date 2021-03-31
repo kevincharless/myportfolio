26affe5bb2f5fb35e5bb60807eb35595dfdb00fc
Kevin Charles <kevinismage@gmail.com>
2021-03-31 13:49:16 +0700

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Home } from './pages';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 const App = ({theme, changeTheme}) => {
   
   return (
-    <>
-      <Home theme={theme} changeTheme={changeTheme} />
-      
-    </>
+
+      <Router>
+        <Switch>
+          <Route><Home theme={theme} changeTheme={changeTheme} /></Route>
+        </Switch>
+      </Router>
   )
 }
 
